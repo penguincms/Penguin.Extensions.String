9398ed43722595989a03968757bda05240a97c2c
Anthony Loffredo <loffredo.ap@gmail.com>
2023-02-12 01:28:10 -0700

    Autocommit
    Autocommit

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -158,30 +158,7 @@ namespace Penguin.Extensions.String
             return result;
         }
 
-        /// <summary>
-        /// Returns the portion of the source string after the first instance of the delimiter
-        /// </summary>
-        /// <param name="s">The source string</param>
-        /// <param name="fromText">The delimiter</param>
-        /// <param name="inclusive">A bool indicating whether or not the delimiter should be returned with the result</param>
-        /// <param name="comparison">The string comparison to use when searching for a match</param>
-        /// <returns>The substring found after the first instance of the delimiter</returns>
-        public static string From(this string s, string fromText, bool inclusive = false, StringComparison comparison = StringComparison.Ordinal)
-        {
-            if (s is null)
-            {
-                return null;
-            }
 
-            if (string.IsNullOrEmpty(fromText))
-            {
-                throw new ArgumentException(EMPTY_STRING_MESSAGE, nameof(fromText));
-            }
-
-            int i = s.IndexOf(fromText, comparison);
-
-            return i >= 0 ? inclusive ? s[i..] : s[(i + fromText.Length)..] : s;
-        }
 
         /// <summary>
         /// Splits a string on \r and \n\ (individually) and returns any "lines" in trimmed form, that are not null or whitespace
@@ -209,50 +186,6 @@ namespace Penguin.Extensions.String
             }
         }
 
-        /// <summary>
-        /// Returns the portion of the source string after the last instance of the delimiter
-        /// </summary>
-        /// <param name="s">The source string</param>
-        /// <param name="fromText">The delimiter</param>
-        /// <returns>The substring found after the last instance of the delimiter</returns>
-        public static string FromLast(this string s, string fromText)
-        {
-            if (s is null)
-            {
-                return null;
-            }
-
-            if (fromText is null)
-            {
-                throw new ArgumentNullException(nameof(fromText));
-            }
-
-            int i = s.LastIndexOf(fromText, StringComparison.OrdinalIgnoreCase);
-
-            return i >= 0 ? s[(i + fromText.Length)..] : s;
-        }
-
-        /// <summary>
-        /// Returns the portion of the source string after the last instance of the delimiter
-        /// </summary>
-        /// <param name="s">The source string</param>
-        /// <param name="fromText">The delimiter</param>
-        /// <returns>The substring found after the last instance of the delimiter</returns>
-        public static string FromLast(this string s, char fromText)
-        {
-            return s?[(s.LastIndexOf(fromText) + 1)..];
-        }
-
-        /// <summary>
-        /// Returns the leftmost portion of a string of a specified length
-        /// </summary>
-        /// <param name="str">The source string</param>
-        /// <param name="count">The number of characters to return</param>
-        /// <returns>A substring of the specified length from the source string</returns>
-        public static string Left(this string str, int count)
-        {
-            return str?[..count];
-        }
 
         /// <summary>
         /// Removes all instances of the specified string, from the source (using Replace)
@@ -284,115 +217,7 @@ namespace Penguin.Extensions.String
             return new string(chars);
         }
 
-        /// <summary>
-        /// Returns the rightmost portion of a string of a specified length
-        /// </summary>
-        /// <param name="str">The source string</param>
-        /// <param name="count">The number of characters to return</param>
-        /// <returns>The rightmost portion of the source string of a specified length</returns>
-        public static string Right(this string str, int count)
-        {
-            return str?[^count..];
-        }
-
-        /// <summary>
-        /// Splits a string on a delimiter
-        /// </summary>
-        /// <param name="input">The source string</param>
-        /// <param name="spliton">The delimiter to split on</param>
-        /// <param name="preserveSplit">A bool indicating whether or not to append the delimiter back to the results</param>
-        /// <param name="options">String split options for the initial split</param>
-        /// <returns>An array of strings</returns>
-        public static string[] Split(this string input, string spliton, bool preserveSplit = false, StringSplitOptions options = StringSplitOptions.None)
-        {
-            if (input is null)
-            {
-                return null;
-            }
-
-            string[] output = input.Split(new string[] { spliton }, options);
-
-            if (preserveSplit)
-            {
-                output = output.Select(s => spliton + s).ToArray();
-            }
-
-            return output;
-        }
-
-        /// <summary>
-        /// Splits a CamelCase string on each uppercase letter by adding a space in front of each
-        /// </summary>
-        /// <param name="str">The string to split</param>
-        /// <returns>A string where each uppercase letter is preceded by a space</returns>
-        public static string SplitCamelCase(this string str)
-        {
-            if (str is null)
-            {
-                return null;
-            }
-
-            int uppers = 0;
-
-            foreach (char c in str.Skip(1))
-            {
-                if (char.IsUpper(c))
-                {
-                    uppers++;
-                }
-            }
-
-            char[] toReturn = new char[str.Length + uppers];
-
-            toReturn[0] = str[0];
-
-            int index = 1;
-            for (int i = 1; i < str.Length; i++)
-            {
-                if (char.IsUpper(str[i]))
-                {
-                    toReturn[index++] = ' ';
-                }
-
-                toReturn[index++] = str[i];
-            }
-
-            return new string(toReturn);
-        }
-
-        [Obsolete("Switch to SplitQuotedString")]
-        public static IEnumerable<string> SplitCSVRow(this string row, char delimiter = ',')
-        {
-            return row.SplitQuotedString(new QuotedStringOptions() { ItemDelimeter = delimiter });
-        }
-
-       
-
-        /// <summary>
-        /// Returns the portion of a string up until the first instance of a delimiter
-        /// </summary>
-        /// <param name="s">The source</param>
-        /// <param name="toText">The delimeter</param>
-        /// <param name="inclusive">A bool indicating whether or not the delimiter should be included in the return</param>
-        /// <param name="comparison">String comparison options</param>
-        /// <returns>The portion of a string up until the first instance of a delimiter</returns>
-        public static string To(this string s, string toText, bool inclusive = false, StringComparison comparison = StringComparison.Ordinal)
-        {
-            if (s is null)
-            {
-                return null;
-            }
-
-            if (string.IsNullOrEmpty(toText))
-            {
-                throw new ArgumentException(EMPTY_STRING_MESSAGE, nameof(toText));
-            }
-
-            int i = s.IndexOf(toText, comparison);
-
-            return i >= 0 ? inclusive ? s[..(i + toText.Length)] : s[..i] : s;
-        }
-
+   
         /// <summary>
         /// Strips non-alphanumeric characters from the string
         /// </summary>
@@ -412,57 +237,7 @@ namespace Penguin.Extensions.String
             return new string(arr);
         }
 
-        /// <summary>
-        /// Splits a string into a dictionary as denoted by the provided K/V separator and KVP delimeter characters
-        /// </summary>
-        /// <param name="source">The source string to split</param>
-        /// <param name="delimeter">The character that separates the key value pairs</param>
-        /// <param name="separator">The character that separates the key and value within the pair</param>
-        /// <returns>A dictionary representing the values</returns>
-        public static Dictionary<string, string> ToDictionary(this string source, char delimeter = ';', char separator = '=')
-        {
-            if (source is null)
-            {
-                throw new ArgumentNullException(nameof(source));
-            }
-
-            if (!source.Contains(separator))
-            {
-                return new Dictionary<string, string>();
-            }
-
-            int eq;
-            int sc;
-
-            if (Vector.IsHardwareAccelerated)
-            {
-                eq = source.VectorizedCount(separator);
-
-                sc = source.VectorizedCount(delimeter);
-            }
-            else
-            {
-                eq = source.Count(c => c == separator);
-
-                sc = source.Count(c => c == delimeter);
-            }
-
-            if (sc != eq - 1 && sc != eq)
-            {
-                return new Dictionary<string, string>();
-            }
-
-            Dictionary<string, string> toReturn = new(eq);
-
-            foreach (string skvp in source.Trim(delimeter).Split(delimeter))
-            {
-                string[] vs = skvp.Split(separator);
-
-                toReturn.Add(vs[0], vs[1]);
-            }
-
-            return toReturn;
-        }
+       
 
         /// <summary>
         /// Converts a string to Base64 encoding
@@ -513,41 +288,6 @@ namespace Penguin.Extensions.String
             return index == 0 ? 0 : int.Parse(new string(toReturn, 0, index));
         }
 
-        /// <summary>
-        /// Returns a portion of a string up to the last instance of a specified delimiter
-        /// </summary>
-        /// <param name="s">The source string</param>
-        /// <param name="toText">The delimiter</param>
-        /// <param name="inclusive">Whether or not to return the delimiter as part of result</param>
-        /// <param name="comparison">The string comparison to use when searching</param>
-        /// <returns>A portion of a string up to the last instance of a specified delimiter</returns>
-        public static string ToLast(this string s, string toText, bool inclusive = false, StringComparison comparison = StringComparison.Ordinal)
-        {
-            if (s is null)
-            {
-                return null;
-            }
-
-            if (toText is null)
-            {
-                throw new ArgumentNullException(nameof(toText));
-            }
-
-            int i = s.LastIndexOf(toText, comparison);
-
-            return i >= 0 ? inclusive ? s[..(i + toText.Length)] : s[..i] : s;
-        }
-
-        /// <summary>
-        /// Returns a portion of a string up to the last instance of a specified delimiter
-        /// </summary>
-        /// <param name="s">The source string</param>
-        /// <param name="toText">The delimiter</param>
-        /// <param name="inclusive">Whether or not to return the delimiter as part of result</param>
-        /// <returns>A portion of a string up to the last instance of a specified delimiter</returns>
-        public static string ToLast(this string s, char toText, bool inclusive = false)
-        {
-            return s?[..(s.LastIndexOf(toText) + (inclusive ? 1 : 0))];
-        }
+      
     }
 }
